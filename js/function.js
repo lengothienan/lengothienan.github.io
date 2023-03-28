958d4631e5193b38211116c87ad9fa0c97583d07
Lê Ngô Thiên Ấn <52607566+lengothienan@users.noreply.github.com>
2023-03-28 21:16:56 +0700

    new

--- js/function.js
+++ js/function.js
@@ -1,10 +1,13 @@
 function CallAPI(){
     $.ajax({
-        url: 'https://apithienan.000webhostapp.com/?id=1',
-        type: 'GET',
+        url: 'https://apithienan.000webhostapp.com/',
+        type: 'POST',
         dataType:'json', 
+        data: {
+            action: "load"
+        },
         success: function(data) {
-        //    $("#main").html(data)
+            console.log(data)
         }
     });
 }
